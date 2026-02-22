39cd2807fb4ea9b114969926fc625726c16881a2
jay5818 <68957356+Jay59376@users.noreply.github.com>
2026-02-23 02:40:06 +0530

    fix: properly add UC3 String.join() refactor

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,11 +1,66 @@
 public class OOPSBannerApp {
+
     public static void main(String[] args) {
-        System.out.println("  *****     *****    ******     *****  ");
-        System.out.println(" *     *   *     *   *     *   *     * ");
-        System.out.println("*       * *       *  *     *   *       ");
-        System.out.println("*       * *       *  ******     *****  ");
-        System.out.println("*       * *       *  *              *  ");
-        System.out.println(" *     *   *     *   *        *     *   ");
-        System.out.println("  *****     *****    *         *****    ");
+
+        System.out.println("OOPS Banner App - UC3: Render using String.join()\n");
+
+        // Line 1
+        System.out.println(String.join(" ",
+                " ***** ",
+                " ***** ",
+                " ******",
+                " ******"
+        ));
+
+        // Line 2
+        System.out.println(String.join(" ",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *"
+        ));
+
+        // Line 3
+        System.out.println(String.join(" ",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*      "
+        ));
+
+        // Line 4
+        System.out.println(String.join(" ",
+                "*     *",
+                "*     *",
+                " ******",
+                " ***** "
+        ));
+
+        // Line 5
+        System.out.println(String.join(" ",
+                "*     *",
+                "*     *",
+                "*      ",
+                "      *"
+        ));
+
+        // Line 6
+        System.out.println(String.join(" ",
+                "*     *",
+                "*     *",
+                "*      ",
+                "*     *"
+        ));
+
+        // Line 7
+        System.out.println(String.join(" ",
+                " ***** ",
+                " ***** ",
+                "*      ",
+                " ***** "
+        ));
     }
 }
+
+
+
