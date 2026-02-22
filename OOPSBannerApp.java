71a4939b7afa023d3a1e88964c483ac1beb69149
jay5818 <68957356+Jay59376@users.noreply.github.com>
2026-02-23 02:58:04 +0530

    feat: refactor banner logic into static helper methods

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,24 +1,57 @@
 class OOPSBannerApp {
 
+    static String[] getOPattern() {
+        return new String[] {
+            " ***** ",
+            "*     *",
+            "*     *",
+            "*     *",
+            "*     *",
+            "*     *",
+            " ***** "
+        };
+    }
+
+    static String[] getPPattern() {
+        return new String[] {
+            "****** ",
+            "*     *",
+            "*     *",
+            "****** ",
+            "*      ",
+            "*      ",
+            "*      "
+        };
+    }
+
+    static String[] getSPattern() {
+        return new String[] {
+            " ***** ",
+            "*      ",
+            "*      ",
+            " ***** ",
+            "      *",
+            "      *",
+            " ***** "
+        };
+    }
+
     public static void main(String[] args) {
+
+        String[] o = getOPattern();
+        String[] p = getPPattern();
+        String[] s = getSPattern();
 
         String gap = "   ";
 
-        String[] banner = {
+        String[] banner = new String[7];
 
-            String.join(gap, " ***** ", " ***** ", " ******", " ***** "),
-            String.join(gap, "*     *", "*     *", "*     *", "*     *"),
-            String.join(gap, "*     *", "*     *", "*     *", "*      "),
-            String.join(gap, "*     *", "*     *", "****** ", " ***** "),
-            String.join(gap, "*     *", "*     *", "*      ", "      *"),
-            String.join(gap, "*     *", "*     *", "*      ", "*     *"),
-            String.join(gap, " ***** ", " ***** ", "*      ", " ***** ")
-
-        };
+        for (int i = 0; i < 7; i++) {
+            banner[i] = String.join(gap, o[i], o[i], p[i], s[i]);
+        }
 
         for (String line : banner) {
             System.out.println(line);
         }
     }
 }
-
